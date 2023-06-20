86c6c72dc4ccc0d82a83f86decd07f43544986b0
felipe <felipeavmuller@gmail.com>
2023-06-20 15:58:38 -0300

    altera smartphone home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,38 @@
+'use client';
+
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import windowOrGlobal from 'window-or-global';
 import styles from './page.module.css';
 
 export default function Home() {
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(windowOrGlobal.innerWidth);
+      setIsImageLoaded(true);
+    }
+
+    handleResize();
+
+    windowOrGlobal.addEventListener('resize', handleResize);
+    return () => windowOrGlobal.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className={ styles.container }>
-      <Image
-        className={ styles.escrito }
-        src="/casulo-azul-escrito.png"
-        alt="CaSulo"
-        width={ 1000 }
-        height={ 1000 }
-      />
+      { isImageLoaded && (
+        <Image
+          className={ styles.escrito }
+          src={ windowWidth < 700 ? '/full-logo.png' : '/casulo-azul-escrito.png' }
+          alt="CaSulo"
+          width={ 1000 }
+          height={ 1000 }
+          key={ windowWidth }
+        />
+      ) }
     </div>
   );
 }
