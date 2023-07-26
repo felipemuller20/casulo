3660f808798edd0453c0de5442db52cab7673f64
felipe <felipeavmuller@gmail.com>
2023-07-26 17:35:28 -0300

    add link about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,12 +6,11 @@ export default function About() {
       <h1>O que somos?</h1>
       <p>
         Somos um laboratório de estudos e pesquisas em carreiras,
-        um centro de inovação com ênfase na sustentabilidade.
-      </p>
-      <p>
-        O CaSuLo é ponto de encontro entre a academia,
+        um centro de inovação com ênfase na sustentabilidade. O CaSuLo é ponto de encontro entre a academia,
         mercado e profissionais para análise das tendências na construção das carreiras contemporâneas.
       </p>
+      <p />
+      <p><a href="http://dgp.cnpq.br/dgp/espelhogrupo/3325001325146203">Grupo de pesquisa certificado pelo CNPq</a></p>
       <h1>Por que "CaSuLo" ?</h1>
       <p>
         O casulo representa um movimento para a transformação e desenvolvimento. Assim como as experiências das trajetórias de carreira, as pesquisas também são dinâmicas,
